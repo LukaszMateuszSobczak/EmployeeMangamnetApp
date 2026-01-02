82bbc655bc2e5a0505310389a0f552ed0551427e
LukaszMateuszSobczak <sobczak.lukasz.mateusz@outlook.com>
2026-01-02 20:47:19 +0100

    Add Excel export for employees using EPPlus

--- EmployeeManagmentApp/Controllers/EmployeeController.cs
+++ EmployeeManagmentApp/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using EmployeeManagmentApp.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 
 
 namespace EmployeeManagmentApp.Controllers
@@ -152,5 +153,62 @@ namespace EmployeeManagmentApp.Controllers
             return View(employees);
         }
 
+        [HttpGet]
+        public IActionResult Export()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ExportToExcel()
+        {
+            var userId = GetUserId();
+            if (userId is null)
+            {
+                return Challenge();
+            }
+            var employees = await _service.GetEmployeesAsync(userId);
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Employees");
+                worksheet.Cells["A1"].Value = "Id";
+                worksheet.Cells["B1"].Value = "Imię";
+                worksheet.Cells["C1"].Value = "Nazwisko";
+                worksheet.Cells["D1"].Value = "Pesel";
+                worksheet.Cells["E1"].Value = "Email";
+                worksheet.Cells["F1"].Value = "Numer telefonu";
+                worksheet.Cells["G1"].Value = "Pensja";
+                worksheet.Cells["H1"].Value = "Data zatrudnienia";
+                worksheet.Cells["I1"].Value = "Miasto";
+                worksheet.Cells["J1"].Value = "Ulica";
+                worksheet.Cells["K1"].Value = "Numer domu";
+                worksheet.Cells["L1"].Value = "Numer mieszkania";
+                worksheet.Cells["M1"].Value = "Kod pocztowy";
+
+
+
+
+                for(var i = 0; i < employees.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = employees[i].Id;
+                    worksheet.Cells[i + 2, 2].Value = employees[i].FirstName;
+                    worksheet.Cells[i + 2, 3].Value = employees[i].LastName;
+                    worksheet.Cells[i + 2, 4].Value = employees[i].Pesel;
+                    worksheet.Cells[i + 2, 5].Value = employees[i].Email;
+                    worksheet.Cells[i + 2, 6].Value = employees[i].Phone
+                    worksheet.Cells[i + 2, 7].Value = employees[i].Salary;
+                    worksheet.Cells[i + 2, 8].Value = employees[i].HireDate;
+                    worksheet.Cells[i + 2, 9].Value = employees[i].Address.City;
+                    worksheet.Cells[i + 2, 10].Value = employees[i].Address.StreetName;
+                    worksheet.Cells[i + 2, 11].Value = employees[i].Address.HouseNumber;
+                    worksheet.Cells[i + 2, 12].Value = employees[i].Address.ApartamentNumber;
+                    worksheet.Cells[i + 2, 13].Value = employees[i].Address.PostalCode;
+                }
+
+                var stream = new MemoryStream(package.GetAsByteArray());
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "employees.xlsx");
+            }
+        }
     }
 }
